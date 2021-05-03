787657566be0183361a6605e23fcdd60a942abcc
Jaqueline Gomez <91078781@mvusd.net>
2021-05-03 15:00:55 -0700

    return btn goes to home screen

--- app/src/main/java/com/example/picscavengerhunt/Leaderboard.java
+++ app/src/main/java/com/example/picscavengerhunt/Leaderboard.java
@@ -3,6 +3,7 @@ package com.example.picscavengerhunt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class Leaderboard extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class Leaderboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leaderboard);
+    }
+    public void returnHomeScreen(View view)
+    {
+        finish();
     }
 }
--- app/src/main/java/com/example/picscavengerhunt/homeScreen.java
+++ app/src/main/java/com/example/picscavengerhunt/homeScreen.java
@@ -2,6 +2,7 @@ package com.example.picscavengerhunt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -13,6 +14,11 @@ public class homeScreen extends AppCompatActivity {
         setContentView(R.layout.activity_home_screen);
     }
 
+    public void onClickGameBtn(View view)
+    {
+        Intent i = new Intent(this, gameScreen.class);
+        startActivity(i);
+    }
     public void returnMainActivity(View view)
     {
         finish();
