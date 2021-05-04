e551208f0ce4b96ab464902275ecac7fa50c0314
Jaqueline Gomez <91078781@mvusd.net>
2021-05-03 19:00:48 -0700

    Camera opens

--- app/src/main/java/com/example/picscavengerhunt/gameScreen.java
+++ app/src/main/java/com/example/picscavengerhunt/gameScreen.java
@@ -1,16 +1,24 @@
 package com.example.picscavengerhunt;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class gameScreen extends AppCompatActivity {
 
+    ImageView imageDisplay;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_screen);
+
     }
 
     public void returnHomeScreen(View view)
@@ -18,4 +26,17 @@ public class gameScreen extends AppCompatActivity {
         finish();
     }
 
+    public void captureImage(View view)
+    {
+        imageDisplay = (ImageView)findViewById(R.id.cameraIV);
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent,0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+        imageDisplay.setImageBitmap(bitmap);
+    }
 }
